a1ba4fa43b0d4d4baa11912c7dd649a24bc229c6
RamitGoolry <rgoolry@ucsd.edu>
2019-11-29 23:41:33 +0200

    New points added and GeoEncoding.py updated (#3)
    * Deleted unnessecary FinalProject.ipynb
    
    There was an extra empty FinalProject.ipynb
    
    * Automated Batch Processing on GeoCoding
    
    * Added 320 new data points
    
    * Deleted Extra GeoCoding files
    
    * Exception Handled and 200 points added
    
    * 500 points added
    
    * Changed markdown to include Locations.csv description
    
    * Added 300 Points
    
    * 300 new points, 1 failed conversion
    
    * 380 new points
    
    * 500 new points
    
    * 500 new points, 1 failed conversion
    
    * 1000 new points, 1 failed conversion
    
    * 1000 new points

--- GeoEncoding.py
+++ GeoEncoding.py
@@ -5,27 +5,52 @@ File to convert addresses in dataframe to coordinates using a Geocoder, so that 
 import pandas as pd
 from tqdm import tqdm
 from pygeocoder import Geocoder
+from pygeolib import GeocoderError
 
+# Geocoder
+MAPS_API_KEY = 'AIzaSyD4ozPjvWdpbW8K3fiabpFwRNSTjITvim8'
+coder = Geocoder(MAPS_API_KEY)
+
+fails = 0
+
+def convert_address(address):
+	global fails
+	
+	try:
+		return coder.geocode(address).coordinates
+	except GeocoderError:
+		fails += 1
+		return (-1, -1) # Invalid Coordinates
+		
 def main():
+	global fails
+	BATCH_SIZE = 1000
+	
 	df = pd.read_csv('Datasets/pd_collisions_datasd_v1.csv')
+	addresses_we_have = pd.read_csv('Datasets/Locations.csv').drop('Unnamed: 0', axis = 1)
+	
+	print(f'Current Size : {addresses_we_have.shape[0]} locations')
+	print(f'Batch Size : {BATCH_SIZE}')
+	
+	df = df[addresses_we_have.shape[0]: min(df.shape[0], addresses_we_have.shape[0] + BATCH_SIZE)] # We only want to geocode the positions we don't have
 	
 	# Extracting Addresses from df
 	addresses = df.apply(lambda x : ' '.join(
 		str(i).strip() for i in [x.address_number_primary, x.address_pd_primary, x.address_road_primary, x.address_sfx_primary] 
 	) + ', SAN DIEGO', axis = 1)
 	
-	# Geocoder
-	MAPS_API_KEY = 'AIzaSyD4ozPjvWdpbW8K3fiabpFwRNSTjITvim8'
-	coder = Geocoder(MAPS_API_KEY)
-	
 	tqdm.pandas() # Progress Bar
 	
 	# Geocoding
 	addresses = addresses.to_frame().rename({0: 'Address'}, axis = 1)
-	addresses['Coordinates'] = addresses['Address'].progress_apply(lambda address : coder.geocode(address).coordinates)
+	addresses['Coordinates'] = addresses['Address'].progress_apply(convert_address)
+	
+	result = pd.concat([addresses_we_have, addresses])
 	
 	# Saving file
-	addresses.to_csv('Datasets/locations.csv')
+	result.to_csv('Datasets/locations.csv')
+	
+	print(f'Completed Batch, New Size : {result.shape[0]} locations \n with {fails} failed conversions.')
 		
 if __name__ == '__main__':
 	main()
